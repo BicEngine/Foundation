5ddc9dc31f16b5798e2c86e38995be3ba0205349
Kirill Nesmeyanov <nesk@xakep.ru>
2023-02-07 10:16:49 +0300

    Fix RunnableInterface namespace

--- src/KernelInterface.php
+++ src/KernelInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Bic\Foundation;
 
 use Bic\Foundation\Exception\HandlerInterface;
-use Bic\Foundation\Kernel\RunnableInterface;
+use Bic\Foundation\RunnableInterface;
 use Psr\Container\ContainerInterface;
 
 interface KernelInterface extends RunnableInterface, HandlerInterface, ContainerInterface
--- src/RunnableInterface.php
+++ src/RunnableInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Bic\Foundation\Kernel;
+namespace Bic\Foundation;
 
 interface RunnableInterface
 {
